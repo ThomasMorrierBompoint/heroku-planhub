ae329c15dfe02119e2e2c1629af41f8cb4a3099c
Morrier-Bompoint Thomas <thomasmorrier@gmail.com>
2017-10-25 09:40:17 -0400

    update

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const server = require('http').Server(app);
 const SERVER_PORT = 5000;
 
-app.get('*', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/public/index.html');
 });
 
 server.listen(process.env.PORT || SERVER_PORT, () => {
