209be2e894cf4cc826dcfa360f5a1da9e5ed69a4
Morrier-Bompoint Thomas <thomasmorrier@gmail.com>
2017-10-25 10:41:10 -0400

    Address auto complete added

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const app = express();
 const server = require('http').Server(app);
 const SERVER_PORT = 5000;
 
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/public/index.html');
+  res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/address-auto-complete', function (req, res) {
+  res.sendFile(__dirname + '/public/components/address-auto-complete/address-auto-complete.html');
 });
 
 server.listen(process.env.PORT || SERVER_PORT, () => {
