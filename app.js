673a549e113d6908c964d6dcb846d386fa2ac874
Morrier-Bompoint Thomas <thomasmorrier@gmail.com>
2017-10-25 09:28:22 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const SERVER_PORT = 5000;
+
+app.get('*', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+server.listen(process.env.PORT || SERVER_PORT, () => {
+  console.log('Express server listening on port %d in %s mode.', SERVER_PORT, app.get('env'));
+});
