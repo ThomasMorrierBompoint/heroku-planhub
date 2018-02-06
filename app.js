557737a0230d108f91b8a65b9718e0387acc90b0
Morrier-Bompoint Thomas <thomasmorrier@gmail.com>
2018-02-06 16:39:06 -0500

    Test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const server = require('http').Server(app);
 const SERVER_PORT = 5000;
 
 const cheerioTest = require('./test/cheerio-test');
-
+test
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
